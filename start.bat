88501326fbd76bab798e678ecde29bf79de7ac73
therealxooxlor <suxurcox@gmail.com>
2015-05-07 16:18:37 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -10,6 +10,30 @@ set ssnid=%IDprefix%
 if %choiceofssnid% == n goto :setssnidtemp
 :setssnidtemp
 echo %ssnid% is your ID.
+echo starting PassTheData...
+echo If you don't know a ip use "$Servers.dat"
+set /p ipofserver=IP:
+if %ipofserver% == $Servers.dat goto :serverdatinstall
+:serverdatinstall
+echo Will add when more servers come out.
+if exist C:/Suns3tNetwork/servers.dat goto :configed
+if NOT exist C:/Suns3tNetwork/servers.dat goto :setupconfiged
+:setupconfiged
+echo NOINSTRUSTIONS!!
+:configed
+ftp -s:C:/Suns3tNetwork/servers.dat %ipofserver%
+Echo Connected
+:menu
+echo host or browse?
+set /p hob=H/b: 
+if %hob% == browse goto :browse
+if %hob% == host goto :host
+:browse
+set /p link=LINK: 
+Echo Searching for %link%...
+set BrowseToken=%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+ftp -s:C:/Suns3tNetwork/%BrowseToken%.Browseforlink
+:host
 echo what file would you like to put in circulation?
 set /p choice=FILE: 
 :zipup
@@ -18,11 +42,3 @@ set counter=counter + 1
 if %counter%==15 goto :zipout
 goto zipup
 :zipout
-echo starting PassTheData...
-set /p ipofserver=IP: 
-if exist C:/Suns3tNetwork/servers.dat goto :configed
-if NOT exist C:/Suns3tNetwork/servers.dat goto :setupconfiged
-:setupconfiged
-echo please install servers file.
-:configed
-ftp -s:C:/Suns3tNetwork/servers.dat %ipofserver%
