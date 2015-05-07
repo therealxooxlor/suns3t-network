8dcf297c09ca723748d558f2ff979a287816b387
therealxooxlor <suxurcox@gmail.com>
2015-05-07 11:16:18 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,7 +1,15 @@
 @echo off
 echo Assigning the ID...
 :gen ssnid
-set ssnid=%random%.%random%.%random%.%random%.%random%.%random%.%random%.%random%.%random%.%random%.%random%.%random%
+echo What do you want your id to be? 
+set /p IDprefix=IDPREFIX: 
+echo Do you want to have a fixed id to host files? MUST BE SAME EVERYTIME OR BROKEN LINKS WILL OCCUR!
+set /p choiceofssnid=Y/n:
+if %choiceofssnid% == Y goto :setfixed
+set ssnid=%IDprefix%
+if %choiceofssnid% == n goto :setssnidtemp
+:setssnidtemp
+echo %ssnid% is your ID.
 echo what file would you like to put in circulation?
 set /p choice=FILE: 
 :zipup
@@ -10,7 +18,11 @@ set counter=counter + 1
 if %counter%==15 goto :zipout
 goto zipup
 :zipout
-echo Sending the file...
 echo starting PassTheData...
-
-
+set /p ipofserver=IP: 
+if exist C:/Suns3tNetwork/servers.dat goto :configed
+if NOT exist C:/Suns3tNetwork/servers.dat goto :setupconfiged
+:setupconfiged
+echo please install servers file.
+:configed
+ftp -s:C:/Suns3tNetwork/servers.dat %ipofserver%
